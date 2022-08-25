6ec8bff349d6666fb2f0960d6a8964ccb725aead
Park JinSoo <ekddl2365@gmail.com>
2022-08-25 20:20:21 +0900

    22/08/25-20:19 Github 연동

---
+++ FirstServlet/src/main/java/first/HelloServlet.java
@@ -0,0 +1,30 @@
+package first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=euc-kr");
+        PrintWriter out=response.getWriter();
+        out.println("<html>"+"<body>"+"<h1>HelloServlet!</h1> "+"</body>"+"</html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
